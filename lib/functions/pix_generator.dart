ba7afbffaf1213427f074403098c9a87e36669d6
Matheus Soares de Sousa Queiroz <dev.matheusqueiroz@gmail.com>
2023-06-06 13:31:46 -0300

    atualizando para colocar valor

--- lib/functions/pix_generator.dart
+++ lib/functions/pix_generator.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 String crc16ccitt(String input) {
   int crc = 0xFFFF;
   int polynomial = 0x1021;
@@ -20,11 +22,12 @@ String crc16ccitt(String input) {
 
 String payLoadEstatico(
     String pKey, String pBenef, String pCity, String pCodTransph,
-    {double? pValue}) {
+    {num? pValue}) {
   const payloadFormatIndicator = '000201';
   const merchantAccountInformation = '26';
   const merchantCategoryCode = '52040000';
   const transactionCurrency = '5303986';
+  const transactionAmount = '54';
   const countryCode = '5802BR';
   const merchantName = '59';
   const merchantCity = '60';
@@ -33,30 +36,57 @@ String payLoadEstatico(
 
   String merchantAccountInformationString =
       '0014BR.GOV.BCB.PIX01${pKey.length}$pKey';
-  // String valorTotal =
-  // pValue == null ? '' : pValue.toStringAsFixed(2).replaceAll(',', '.');
+  String valorTotal =
+      pValue == null ? '' : pValue.toStringAsFixed(2).replaceAll(',', '.');
   String txid =
       '05${pCodTransph.length.toString().padLeft(2, '0')}$pCodTransph';
 
-  String codPayLoad = payloadFormatIndicator +
-      merchantAccountInformation +
-      merchantAccountInformationString.length.toString() +
-      merchantAccountInformationString +
-      merchantCategoryCode +
-      transactionCurrency +
-      countryCode +
-      merchantName +
-      pBenef.length.toString().padLeft(2, '0') +
-      pBenef +
-      merchantCity +
-      pCity.length.toString().padLeft(2, '0') +
-      pCity +
-      additionalDataFieldTemplate +
-      txid.length.toString().padLeft(2, '0') +
-      txid +
-      crc16;
+  String codPayLoad = '';
+
+  if (pValue != null) {
+    codPayLoad = payloadFormatIndicator +
+        merchantAccountInformation +
+        merchantAccountInformationString.length.toString() +
+        merchantAccountInformationString +
+        merchantCategoryCode +
+        transactionCurrency +
+        transactionAmount +
+        valorTotal.length.toString().padLeft(2, '0') +
+        valorTotal +
+        countryCode +
+        merchantName +
+        pBenef.length.toString().padLeft(2, '0') +
+        pBenef +
+        merchantCity +
+        pCity.length.toString().padLeft(2, '0') +
+        pCity +
+        additionalDataFieldTemplate +
+        txid.length.toString().padLeft(2, '0') +
+        txid +
+        crc16;
+  } else {
+    codPayLoad = payloadFormatIndicator +
+        merchantAccountInformation +
+        merchantAccountInformationString.length.toString() +
+        merchantAccountInformationString +
+        merchantCategoryCode +
+        transactionCurrency +
+        countryCode +
+        merchantName +
+        pBenef.length.toString().padLeft(2, '0') +
+        pBenef +
+        merchantCity +
+        pCity.length.toString().padLeft(2, '0') +
+        pCity +
+        additionalDataFieldTemplate +
+        txid.length.toString().padLeft(2, '0') +
+        txid +
+        crc16;
+  }
 
   String crc = crc16ccitt(codPayLoad);
+
+  log(codPayLoad + crc);
 
   return codPayLoad + crc;
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,134 +18,154 @@ class _HomePageState extends State<HomePage> {
   final TextEditingController _chavePix = TextEditingController();
   final TextEditingController _nome = TextEditingController();
   final TextEditingController _cidade = TextEditingController();
+  final TextEditingController _valor = TextEditingController();
   String? selectedPixKey;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            DropdownButtonFormField<String>(
-              value: selectedPixKey,
-              hint: const Text('Selecione o tipo de chave Pix'),
-              onChanged: (String? newValue) {
-                _chavePix.clear();
-                setState(() {
-                  selectedPixKey = newValue!;
-                });
-              },
-              items: <String>['Telefone', 'Email', 'CPF/CNPJ', 'Outro']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            const SizedBox(height: 20),
-            if (selectedPixKey == 'Telefone')
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Chave Pix',
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              DropdownButtonFormField<String>(
+                value: selectedPixKey,
+                hint: const Text('Selecione o tipo de chave Pix'),
+                onChanged: (String? newValue) {
+                  _chavePix.clear();
+                  setState(() {
+                    selectedPixKey = newValue!;
+                  });
+                },
+                items: <String>['Telefone', 'Email', 'CPF/CNPJ', 'Outro']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              const SizedBox(height: 20),
+              if (selectedPixKey == 'Telefone')
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Chave Pix',
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    TelefoneInputFormatter(),
+                  ],
+                  controller: _chavePix,
                 ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  TelefoneInputFormatter(),
-                ],
-                controller: _chavePix,
-              ),
-            if (selectedPixKey == 'Email')
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Chave Pix',
+              if (selectedPixKey == 'Email')
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Chave Pix',
+                  ),
+                  controller: _chavePix,
                 ),
-                controller: _chavePix,
-              ),
-            if (selectedPixKey == 'CPF/CNPJ')
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Chave Pix',
+              if (selectedPixKey == 'CPF/CNPJ')
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Chave Pix',
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly,
+                    CpfOuCnpjFormatter(),
+                  ],
+                  controller: _chavePix,
                 ),
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                  CpfOuCnpjFormatter(),
-                ],
-                controller: _chavePix,
-              ),
-            if (selectedPixKey == 'Outro')
-              TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Chave Pix',
+              if (selectedPixKey == 'Outro')
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Chave Pix',
+                  ),
+                  controller: _chavePix,
                 ),
-                controller: _chavePix,
+              TextFormField(
+                maxLength: 25,
+                decoration: const InputDecoration(
+                  labelText: 'Nome',
+                ),
+                controller: _nome,
               ),
-            TextFormField(
-              maxLength: 25,
-              decoration: const InputDecoration(
-                labelText: 'Nome',
+              TextFormField(
+                maxLength: 15,
+                decoration: const InputDecoration(
+                  labelText: 'Cidade',
+                ),
+                controller: _cidade,
               ),
-              controller: _nome,
-            ),
-            TextFormField(
-              maxLength: 15,
-              decoration: const InputDecoration(
-                labelText: 'Cidade',
+              TextFormField(
+                maxLength: 13,
+                decoration: const InputDecoration(
+                  labelText: 'Valor',
+                ),
+                controller: _valor,
+                keyboardType: const TextInputType.numberWithOptions(
+                    decimal: true, signed: true),
               ),
-              controller: _cidade,
-            ),
-            const SizedBox(height: 50),
-            Center(
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: MediaQuery.of(context).size.height * 0.05,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (_chavePix.text.isEmpty ||
-                        _nome.text.isEmpty ||
-                        _cidade.text.isEmpty) {
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text('Preencha todos os campos'),
-                        duration: Duration(seconds: 2),
-                      ));
-                      return;
-                    }
-
-                    String chavepixatt = _chavePix.text;
-
-                    if (selectedPixKey == 'Telefone') {
-                      chavepixatt =
-                          '+55${UtilBrasilFields.obterTelefone(chavepixatt, mascara: false)}';
-                    }
-
-                    if (selectedPixKey == 'CPF/CNPJ') {
-                      if (chavepixatt.contains('/')) {
-                        chavepixatt = chavepixatt.replaceAll('.', '');
-                        chavepixatt = chavepixatt.replaceAll('-', '');
-                        chavepixatt = chavepixatt.replaceAll('/', '');
-                      } else {
-                        chavepixatt = chavepixatt.replaceAll('.', '');
-                        chavepixatt = chavepixatt.replaceAll('-', '');
+              const SizedBox(height: 50),
+              Center(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_chavePix.text.isEmpty ||
+                          _nome.text.isEmpty ||
+                          _cidade.text.isEmpty) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text('Preencha todos os campos'),
+                          duration: Duration(seconds: 2),
+                        ));
+                        return;
                       }
-                    }
 
-                    log(chavepixatt);
+                      String chavepixatt = _chavePix.text;
 
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => PixPage(
-                            pix: payLoadEstatico(
-                                chavepixatt, _nome.text, _cidade.text, '***')),
-                      ),
-                    );
-                  },
-                  child: const Text('PIX'),
+                      if (selectedPixKey == 'Telefone') {
+                        chavepixatt =
+                            '+55${UtilBrasilFields.obterTelefone(chavepixatt, mascara: false)}';
+                      }
+
+                      if (selectedPixKey == 'CPF/CNPJ') {
+                        if (chavepixatt.contains('/')) {
+                          chavepixatt = chavepixatt.replaceAll('.', '');
+                          chavepixatt = chavepixatt.replaceAll('-', '');
+                          chavepixatt = chavepixatt.replaceAll('/', '');
+                        } else {
+                          chavepixatt = chavepixatt.replaceAll('.', '');
+                          chavepixatt = chavepixatt.replaceAll('-', '');
+                        }
+                      }
+
+                      num? valor;
+
+                      if (_valor.text.isNotEmpty) {
+                        valor = num.parse(_valor.text.replaceAll(',', '.'));
+                      }
+
+                      log(chavepixatt);
+
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => PixPage(
+                              pix: payLoadEstatico(
+                                  chavepixatt, _nome.text, _cidade.text, '***',
+                                  pValue: valor)),
+                        ),
+                      );
+                    },
+                    child: const Text('PIX'),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
